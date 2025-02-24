ac724124fd283b33fb3a9c296a97de81e9a9c4c5
Hemasri Guthula <guthulahemasri410@gmail.com>
2025-02-24 16:11:39 +0530

    Update serviceCard.jsx

--- service-listing/src/components/serviceCard.jsx
+++ service-listing/src/components/serviceCard.jsx
@@ -1,27 +1,20 @@
 import React from "react";
 
-
-function serviceCard ({data}) {
+const ServiceCard = ({ data }) => {
     return (
         <div>
-           {
-            data.length > 0 ? (
-                data.map((el,i) => (
-                    <div key = {i}>
+            {data.length > 0 ? (
+                data.map((el, i) => (
+                    <div key={i}>
                         <h1>service title: {el.name}</h1>
                         <h1>service description: {el.title}</h1>
-                        
                     </div>
-
-
                 ))
-
-            ):(
+            ) : (
                 <p>Loading...</p>
-            )
-           }
-            
+            )}
         </div>
-    )
-}
-export default serviceCard
+    );
+};
+
+export default ServiceCard;
